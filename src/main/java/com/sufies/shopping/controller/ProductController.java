e7c918675224c41074dafcfde068cafdd0a71d38
sadikta <sadhiqbinali@gmail.com>
2021-07-05 14:57:56 +0530

    Initial commit

---
+++ src/main/java/com/sufies/shopping/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.sufies.shopping.controller;
+
+import com.sufies.shopping.model.Product;
+import com.sufies.shopping.model.User;
+import com.sufies.shopping.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/addProduct")
+    public String addProduct(ModelMap model){
+        model.addAttribute("product",new Product());
+        return "addproduct";
+    }
+    @PostMapping("/addProduct")
+    public String saveProduct(Product product){
+        productService.saveProduct(product);
+         return "redirect:/productlist";
+    }
+    @GetMapping("/productlist")
+    public  String viewProductList(ModelMap model){
+        model.put("product" , productService.getAllproducts());
+        return "productlist" ;
+    }
+
+    @GetMapping("/deleteproduct")
+    public String deleteProduct(long id){
+        productService.deleteProduct(id);
+        return "redirect:/productlist";
+
+    }
+    @GetMapping("/updateproduct")
+    public String updateProduct(@RequestParam long id,ModelMap model){
+        Product product = productService.findById(id).get();
+        Long idprint = product.getpId();
+        System.out.println("id printed in getmap"+idprint);
+        model.put("product",product);
+        return "updateproduct";
+    }
+    @PostMapping("/updateproduct")
+    public  String updateProductSave(Product product){
+        System.out.println("value"+product.getpId()+product.getPname()+product.getPcolor()+product.getPtype()+product.getPamount());
+        productService.updateProduct(product);
+
+        return "redirect:/productlist";
+    }
+
+
+
+}
